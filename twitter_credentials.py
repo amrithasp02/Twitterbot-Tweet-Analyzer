39be842018958cbb02db4c25c3f721381af7fabe
amrithasp02 <63729021+amrithasp02@users.noreply.github.com>
2021-04-25 15:35:44 +0530

    Credentials for Authentication

---
+++ twitter_credentials.py
@@ -0,0 +1,4 @@
+ckey = 'WApESZGzrLJcF1Yjqrzboq8GU'
+csecret = 'AzpnCkUZQ0sJh6aidbAkokvIH5xsL23kT2lKfBZlQwWLjezFYP'
+atoken = '1058957021482024960-XbwWuXQIcU0O36EoFpeCrsdDdta18j'
+asecret = 'SocKc9gTMvmjS4eBdx2bmORYUlEWXNsvWNQdgfBoZ7R2S'
